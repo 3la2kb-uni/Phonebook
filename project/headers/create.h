1fddc86cfb0c8c4447a965598c7420677f716d20
root <root@localhost.localdomain>
2019-12-20 10:03:46 -0500

    upload initial

---
+++ project/headers/create.h
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "general.h"
+#include <dirent.h>
+
+
+int create_table(char name[]){
+
+DIR *d;
+struct dirent *dir;
+d = opendir("./database");
+FILE * fPtr;
+
+if (d){
+        while ((dir = readdir(d)) != NULL)
+
+        {
+
+	if(!(strcmp(dir->d_name,name)))
+{
+
+		printf("Table name already exists.\n");
+		return 0;
+		}
+
+     }
+
+        closedir(d);
+
+
+
+
+
+
+}
+fPtr = fopen(concat("database/",name), "w");
+
+if(fPtr == NULL)
+	{
+	printf("Table is not created.\n");
+	return 0;
+	}
+else{
+	fclose(fPtr);
+        printf("Table created successfully.\n");
+	return 1;
+}
+
+
+}
+
